2c5ef39f47b5264abd8c0799c0eb40163bc94c84
dato200026 <dattotsintsadze@gmail.com>
2023-07-17 16:37:02 +0400

    Added index.html

--- static/js/burger-menu.js
+++ static/js/burger-menu.js
@@ -2,11 +2,19 @@ const burgerMenu=document.querySelector('.burger-menu-icon');
 const navList=document.querySelector('.header-nav-ul');
 burgerMenu.addEventListener('click',() =>{
     navList.classList.toggle('header-nav-height');
-    const navHeight=navList.getBoundingClientRect().height;
+    let navHeight=navList.getBoundingClientRect().height;
+
     if(navHeight>0){
         document.querySelector("section").style.marginTop=`${navHeight}px`;
     }
     else{
         document.querySelector("section").style.marginTop=`0`;
     }
+
+    window.addEventListener('resize',() =>{
+     if(window.innerWidth > 768){
+         navList.classList.remove('header-nav-height');
+         document.querySelector("section").style.marginTop=`0`;
+     }
+    });
 });
